72a98ba444c57ff631b4697868c1b78390d16e62
Andrew Jarvis <ajarvis7771@gmail.com>
2020-12-29 17:38:12 -0500

    new file structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './app/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/js'),
     filename: "bundle.js"
   },
   module: {
